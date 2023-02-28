3ba6808bdc6cefbdd49bef368233b0e6100070f8
Gimena Medrano <fmedranop1@miumg.edu.gt>
2023-02-28 12:14:28 -0600

    if>0

--- Program.cs
+++ Program.cs
@@ -6,30 +6,35 @@ try
 
     Console.WriteLine("inrese un numero entero mayor a 0");
     N = Convert.ToUInt16(Console.ReadLine());
-
-    for (ushort i = 1; i <= N; i++)
+    if (N > 0)
     {
-        factorial *= i; //calcula el factorial de un numero 
-
-        if ((i <= N) && (i % 3 == 0))
+        for (ushort i = 1; i <= N; i++)
         {
-            Console.WriteLine($"los valores menores o iguales y divisibles en 3 del numero ingresado es {i}");
+            factorial *= i; //calcula el factorial de un numero 
+
+            if ((i <= N) && (i % 3 == 0))
+            {
+                Console.WriteLine($"los valores menores o iguales y divisibles en 3 del numero ingresado es {i}");
+
+            }
+            if ((i <= N) && (i % 5 == 0))
+            {
+
+                Console.WriteLine($"los valores menores o iguales y divisibles en 5 del numero ingresado es {i}");
+
+            }
+            if (m <= N)
+            {
+                m += 2;//incremento en 2
+                Console.WriteLine(" números enteros positivos que comienzan en 1 y se incrementan en 2 hasta que se alcance un valor mayor que N es " + m);
+            }
 
         }
-        if ((i <= N) && (i % 5 == 0))
-        {
 
-            Console.WriteLine($"los valores menores o iguales y divisibles en 5 del numero ingresado es {i}");
-
-        }
-        if (m <= N)
-        {
-            m += 2;//incremento en 2
-            Console.WriteLine(" números enteros positivos que comienzan en 1 y se incrementan en 2 hasta que se alcance un valor mayor que N es " + m);
-        }
-
+        Console.WriteLine($"el  {N}! es {factorial}");
     }
-    Console.WriteLine($"el  {N}! es {factorial}");
+    else
+    { Console.WriteLine("ingrese un valor mayor a 0");}
 }
 catch (Exception e)
 {
